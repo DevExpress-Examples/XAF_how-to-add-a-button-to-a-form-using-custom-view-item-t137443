bc2d492655a83a87826366e3ce920ebc6b84ff97
andrey <andrey.kozhevnikov@devexpress.com>
2023-06-07 17:58:25 +0400

    EF

---
+++ CS/EFCore/ButtonInFormEF/ButtonInFormEF.Module/BusinessObjects/ButtonInFormEFDbContext.cs
@@ -0,0 +1,44 @@
+﻿using DevExpress.ExpressApp.EFCore.Updating;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+using MySolution.Module.BusinessObjects;
+
+namespace ButtonInFormEF.Module.BusinessObjects;
+
+// This code allows our Model Editor to get relevant EF Core metadata at design time.
+// For details, please refer to https://supportcenter.devexpress.com/ticket/details/t933891.
+public class ButtonInFormEFContextInitializer : DbContextTypesInfoInitializerBase {
+	protected override DbContext CreateDbContext() {
+		var optionsBuilder = new DbContextOptionsBuilder<ButtonInFormEFEFCoreDbContext>()
+            .UseSqlServer(";")
+            .UseChangeTrackingProxies()
+            .UseObjectSpaceLinkProxies();
+        return new ButtonInFormEFEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+//This factory creates DbContext for design-time services. For example, it is required for database migration.
+public class ButtonInFormEFDesignTimeDbContextFactory : IDesignTimeDbContextFactory<ButtonInFormEFEFCoreDbContext> {
+	public ButtonInFormEFEFCoreDbContext CreateDbContext(string[] args) {
+		throw new InvalidOperationException("Make sure that the database connection string and connection provider are correct. After that, uncomment the code below and remove this exception.");
+		//var optionsBuilder = new DbContextOptionsBuilder<ButtonInFormEFEFCoreDbContext>();
+		//optionsBuilder.UseSqlServer("Integrated Security=SSPI;Pooling=false;Data Source=(localdb)\\mssqllocaldb;Initial Catalog=ButtonInFormEF");
+        //optionsBuilder.UseChangeTrackingProxies();
+        //optionsBuilder.UseObjectSpaceLinkProxies();
+		//return new ButtonInFormEFEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+[TypesInfoInitializer(typeof(ButtonInFormEFContextInitializer))]
+public class ButtonInFormEFEFCoreDbContext : DbContext {
+	public ButtonInFormEFEFCoreDbContext(DbContextOptions<ButtonInFormEFEFCoreDbContext> options) : base(options) {
+	}
+    //public DbSet<ModuleInfo> ModulesInfo { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+    }
+}
